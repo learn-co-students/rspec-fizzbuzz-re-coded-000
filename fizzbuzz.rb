9944aff9366878bbadb9d75ea96ec2251c429bee
Noor Sulaiman <noor.s.suliman@gmail.com>
2016-09-28 14:57:52 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz (x)
+
+  if (x % 3 == 0) && (x % 5 != 0) == true
+  "Fizz"
+
+elsif (x % 5 == 0) && (x % 3 != 0) == true
+
+
+     "Buzz"
+
+  elsif (x % 3 == 0) && (x % 5 == 0) == true
+
+
+     "FizzBuzz"
+
+  else
+    puts " "
+
+  end
+end
